673c7324dc8370056d86bc3510158a6cfb66d1de
Chanidapaka <chanidapapanta@gmail.com>
2025-03-29 23:07:28 +0700

    Add file AuthUserDetail

--- Downloads/204/บทที่8/jwt-demo/src/main/java/sit/int204/jwtdemo/entities/configs/WebSecurityConfig.java
+++ Downloads/204/บทที่8/jwt-demo/src/main/java/sit/int204/jwtdemo/entities/configs/WebSecurityConfig.java
@@ -7,20 +7,34 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-@Configuration
+//ซึ่งใช้ในการตั้งค่าการควบคุมการเข้าถึง (access control) สำหรับแอปพลิเคชันที่ใช้ Spring Boot โดยตั้งค่าต่างๆ ผ่าน HttpSecurity ใน Spring Security
+@EnableWebSecurity //ใช้ในการเปิดใช้งาน Spring Security ในแอปพลิเคชัน ทำให้สามารถตั้งค่าความปลอดภัยของเว็บแอปพลิเคชันได้
+@Configuration //ที่ใช้ในการกำหนดการตั้งค่าต่างๆ ของ Spring
 public class WebSecurityConfig {
-    @Bean
+
+    //การใช้ HttpSecurity ช่วยให้สามารถกำหนดกฎการอนุญาต (authorization) การป้องกัน CSRF, และการจัดการ session ได้
+    @Bean //ที่ใช้กำหนดค่าการตั้งค่า HttpSecurity ซึ่งเป็นตัวควบคุมการตั้งค่าความปลอดภัยของแอปพลิเคชัน
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //ปิดการตั้งค่า frame options เพื่อไม่ให้ error
         http.headers(httpSecurityHeadersConfigurer ->
                 httpSecurityHeadersConfigurer.frameOptions(frameOptionsConfig ->
                         frameOptionsConfig.disable()));
+
+        //ปิดการตรวจสอบ Cross-Site Request Forgery (CSRF) ซึ่งปกติแล้ว Spring Security จะเปิดใช้งาน CSRF protection
+        //แต่ในกรณีนี้ปิดการป้องกัน CSRF เนื่องจากอาจใช้ API แบบ stateless หรือการตั้งค่าอื่นๆ ที่ไม่ต้องการ CSRF protection
         http.csrf(crsf -> crsf.disable())
+
+                //กำหนดให้ทุกคำขอ (request) ในแอปพลิเคชันสามารถเข้าถึงได้โดยไม่ต้องมีการตรวจสอบสิทธิ์ (ทุกหน้าสามารถเข้าถึงได้ไม่จำกัด)
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().permitAll()
                 )
+
+                //กำหนด session ให้เป็น stateless คือแอปพลิเคชันจะไม่เก็บสถานะของ session ระหว่างคำขอ (request) ทุกคำขอจะไม่ถูกผูกกับ session ใดๆ
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //สร้างและคืนค่าการตั้งค่าความปลอดภัยที่ถูกกำหนดใน http เพื่อให้ Spring Security ใช้ในการรักษาความปลอดภัยของแอปพลิเคชัน
         return http.build();
     }
 }
--- Downloads/204/บทที่8/jwt-demo/src/main/java/sit/int204/jwtdemo/entities/service/UserService.java
+++ Downloads/204/บทที่8/jwt-demo/src/main/java/sit/int204/jwtdemo/entities/service/UserService.java
@@ -14,30 +14,38 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepo;
+    //ถ้าไม่มีข้อมูลซ้ำ จะมีการเข้ารหัสรหัสผ่านของผู้ใช้โดยใช้ Argon2PasswordEncoder และบันทึกข้อมูลผู้ใช้ลงในฐานข้อมูล
     private Argon2PasswordEncoder passwordEncoder =
-            new Argon2PasswordEncoder(16, 32, 8, 1024*128, 2);
+            new Argon2PasswordEncoder(
+                    16, 16,
+                    8, 1024*128, 2);
 
+    //ฟังก์ชันนี้ใช้ค้นหาผู้ใช้จากฐานข้อมูลโดยใช้ id
+    //ถ้าพบผู้ใช้ที่มี id ตรงกับที่ส่งเข้ามา จะคืนค่าผู้ใช้ (User) ที่ตรงกัน  //ถ้าไม่พบผู้ใช้ จะคืนค่าเป็น null
     public User findUserById(Long id) {
         return userRepo.findById(id).orElse(null);
     }
 
+    //ฟังก์ชันนี้ใช้ตรวจสอบว่า username หรือ email ของผู้ใช้มีอยู่ในฐานข้อมูลแล้วหรือไม่ โดยเรียกใช้ userRepo.existsUserByUsernameOrEmail()
     private void checkDuplication(User user) {
         if (userRepo.existsUserByUsernameOrEmail(
                 user.getUsername(), user.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, //ถ้ามีข้อมูลซ้ำ จะโยน"User name or Email already exist !!!
                     "User name or Email already exist !!! ("
                             + user.getUsername() + ", " + user.getEmail() + ')');
         }
     }
+        //ฟังก์ชันนี้ใช้ในการสร้างผู้ใช้ใหม่
         public User createUser(User user) {
-            checkDuplication(user);
+            checkDuplication(user); //เพื่อเช็คว่ามีผู้ใช้ที่มี username หรือ email ซ้ำกันหรือไม่
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             return userRepo.save(user);
         }
 
+        //ฟังก์ชันนี้ใช้ในการสร้างผู้ใช้หลายคนในครั้งเดียว
         public List<User> createUsers(List<User> users) {
             for (User user : users) {
-                checkDuplication(user);
+                checkDuplication(user); //เพื่อเช็คว่ามีผู้ใช้ที่มี username หรือ email ซ้ำกันหรือไม่
                 user.setPassword(passwordEncoder.encode(user.getPassword()));
             }
             return userRepo.saveAll(users);
